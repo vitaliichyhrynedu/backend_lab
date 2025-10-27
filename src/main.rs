ac2154cc7b56c85dc3faf7e6dbc5bdaed1784888
Vitalii Chyhryn <chyhryn.vitalii+edu@gmail.com>
2025-10-28 00:01:03 +0200

    feat: implement establishing a database connection

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,31 @@
 use std::env;
 
-use crate::database::Database;
+use sea_orm::{Database, DatabaseConnection};
 
-mod database;
 mod models;
 mod routes;
 
 #[derive(Clone)]
 struct AppState {
-    db: Database,
+    db: DatabaseConnection,
 }
 
 #[tokio::main]
 async fn main() {
     dotenvy::dotenv().ok();
 
-    let db = database::create().await;
+    let db_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let db = Database::connect(&db_url)
+        .await
+        .expect("database connection failed");
+    eprintln!("database connection established");
+
     let state = AppState { db };
     let router = routes::router().with_state(state);
 
     let port = env::var("PORT").expect("PORT must be set");
-    let address = format!("0.0.0.0:{port}");
-    let listener = tokio::net::TcpListener::bind(address).await.unwrap();
+    let addr = format!("0.0.0.0:{port}");
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
     eprintln!("listening on http://localhost:{port}");
 
     axum::serve(listener, router).await.unwrap();
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -2,23 +2,22 @@ use axum::{Router, routing::get};
 
 use crate::AppState;
 
-mod categories;
-mod health;
-mod records;
-mod users;
+// mod categories;
+// mod health;
+// mod records;
+// mod users;
 
 pub fn router() -> Router<AppState> {
-    let health = health::router();
-    let users = users::router();
-    let categories = categories::router();
-    let records = records::router();
+    // let health = health::router();
+    // let users = users::router();
+    // let categories = categories::router();
+    // let records = records::router();
 
-    Router::new()
-        .route("/", get(get_root))
-        .nest("/health", health)
-        .nest("/users", users)
-        .nest("/categories", categories)
-        .nest("/records", records)
+    Router::new().route("/", get(get_root))
+    // .nest("/health", health)
+    // .nest("/users", users)
+    // .nest("/categories", categories)
+    // .nest("/records", records)
 }
 
 async fn get_root() -> &'static str {
