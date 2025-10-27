4634c72ae2f362263acd188e340f126ddde78ec6
Vitalii Chyhryn <chyhryn.vitalii+edu@gmail.com>
2025-10-28 01:46:46 +0200

    chore: create categories table

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -1,12 +1,16 @@
 pub use sea_orm_migration::prelude::*;
 
 mod m20251027_233718_create_users_table;
+mod m20251027_234532_create_categories_table;
 
 pub struct Migrator;
 
 #[async_trait::async_trait]
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
-        vec![Box::new(m20251027_233718_create_users_table::Migration)]
+        vec![
+            Box::new(m20251027_233718_create_users_table::Migration),
+            Box::new(m20251027_234532_create_categories_table::Migration),
+        ]
     }
 }
