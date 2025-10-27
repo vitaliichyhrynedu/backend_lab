a8195aaa6a402f8198b6d82a7d891dc02294a98c
Vitalii Chyhryn <chyhryn.vitalii+edu@gmail.com>
2025-10-27 18:11:38 +0200

    refactor: use PORT env var

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
+use std::env;
+
 mod routes;
 
 #[tokio::main]
 async fn main() {
+    dotenvy::dotenv().ok();
+
     let router = routes::router();
-    let address = "0.0.0.0:8080";
+
+    let port = env::var("PORT").expect("PORT must be set");
+    let address = format!("0.0.0.0:{port}");
+
     let listener = tokio::net::TcpListener::bind(address).await.unwrap();
     axum::serve(listener, router).await.unwrap();
 }
